57904e06c4460892941306820c80d38afb1617d1
Kendall Brown <kendallbrwn@gmail.com>
2021-05-07 22:54:15 -0600

    Wrote basic program structure

--- src/Bag.java
+++ src/Bag.java
@@ -1,34 +1,35 @@
-
-public abstract class Bag {
-	private float bag_probability;
-	private int c_probability;
-	private int l_probability;
+public class Bag {
+	private double bag_probability;
+	private double c_probability;
+	private double l_probability;
 	
-	public Bag(float bag, int c, int l) {
+	public Bag(double bag, double c, double l) {
 		bag_probability = bag;
 		c_probability = c;
 		l_probability = l;
 	}
 	
-	public void update_bag_probability(char candy, float candy_probability) {
-		int candy_given_bag;
+	public void update_bag_probability(char candy, double candy_probability) {
+		double candy_given_bag;
 		
 		if (candy == 'C') {
 			candy_given_bag = c_probability;
 		} else {
 			candy_given_bag = l_probability;
 		}
+		
+		bag_probability = candy_given_bag * bag_probability / candy_probability;
 	}
 	
-	public float get_bag_probability() {
+	public double get_bag_probability() {
 		return bag_probability;
 	}
 	
-	public int get_c_probability() {
+	public double get_c_probability() {
 		return c_probability;
 	}
 	
-	public int get_l_probability() {
+	public double get_l_probability() {
 		return l_probability;
 	}
 	
